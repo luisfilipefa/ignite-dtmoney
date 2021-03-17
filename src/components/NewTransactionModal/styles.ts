4e8bc8799ad3bd2528ce7357ac1acc0e246c7cd8
luisfilipefa <luisfilipe.faw@gmail.com>
2021-03-17 15:34:01 -0300

    removed curly brackets left behind

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -65,32 +65,31 @@ const colors = {
 };
 
 export const RadioBox = styled.button<RadioBoxProps>`
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: ${(props) =>
-      props.isActive
-        ? transparentize(0.8, colors[props.activeColor])
-        : "transparent"};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: border-color 0.2s;
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.8, colors[props.activeColor])
+      : "transparent"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
 
-    &:hover {
-      border-color: ${darken(0.1, "#d7d7d7")};
-    }
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
